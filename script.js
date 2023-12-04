d27ab990effd8b5564493b23dd7d013f28d8933a
Yuvraj Maurya <yuvraj95166@gmail.com>
2023-12-04 21:56:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,3 +6,78 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+const playbackSpeed = player.querySelector('input[name="playbackRate"]');
+const volume = player.querySelector('input[name="volume"]');
+const back10s = player.querySelector('.back-10');
+const forward25s = player.querySelector('.forward-25');
+
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+}
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+function changePlaybackSpeed() {
+  video.playbackRate = this.value;
+}
+
+video.addEventListener('click', togglePlay);
+toggle.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+progress.addEventListener('click', scrub);
+let mousedown = false;
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+playbackSpeed.addEventListener('input', changePlaybackSpeed);
+volume.addEventListener('input', handleRangeUpdate);
+
+function skipBack10s() {
+  video.currentTime -= 10;
+}
+
+function skipForward25s() {
+  video.currentTime += 25;
+}
+
+back10s.addEventListener('click', skipBack10s);
+forward25s.addEventListener('click', skipForward25s);
+
